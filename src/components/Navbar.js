7263f7361f0a38ed6ddfc05fd0a7d9e81b3f8932
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-06-28 22:15:29 -0400

    created unordered list for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
     return (
-        <div>
-            
+        <div className="navbar">
+        <div className="logo">Ishwor Movie App</div>
+        <div className="menus">
+            <ul className="menu">
+
+            </ul>
+        </div>
         </div>
     )
 }
