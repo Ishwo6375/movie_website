1c25a82e19f7e6f7baf826ff2298335bf4440049
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-06-28 22:29:03 -0400

    created components watchlist , contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import react from "react"
+import react from "react";
 import "./App.css";
 import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-   <Home />
-  </div>
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+           <Route path="/contact">
+            <Contact />
+          </Route>
+           <Route path="/watchlist">
+            <Watchlist />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,9 @@ function Navbar() {
         <div className="logo">Ishwor Movie App</div>
         <div className="menus">
             <ul className="menu">
-
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/watchlist'>Watchlist</Link></li>
+              <li><Link to='/contact'>Contact</Link></li>
             </ul>
         </div>
         </div>
