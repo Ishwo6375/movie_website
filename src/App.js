5c85eb348b3e796f2d3001733a8857099fc1618b
ishwo6375 <ishaan.prazapati6375@gmail.com>
2021-06-28 22:06:50 -0400

    created home.js, contact.js, navbar.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import react from "react"
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return <div className="App">
-    App component
+   <Home />
   </div>
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Home() {
+function Navbar() {
     return (
         <div>
-            Home component
+            
         </div>
     )
 }
 
-export default Home
+export default Navbar
